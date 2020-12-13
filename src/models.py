6d595574bffd87b6e14697758f788f29c853312e
ahmed-asiri <ia7mad13@gmail.com>
2020-12-13 03:17:27 +0300

    Initial

--- src/models.py
+++ src/models.py
@@ -19,6 +19,7 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=database_path):
+    app.config.from_object('config')
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
